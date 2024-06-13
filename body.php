980adac4bb55c70a7ade5f8ab42aeb08a3ea113f
Gyamfi John <115163376+gyamficodes@users.noreply.github.com>
2024-06-13 06:52:46 -0700

    project frontend

--- body.php
+++ body.php
@@ -1,11 +1,29 @@
 <div class="container">
-<nav  class="navbar  navbar-light bg-light">
+<nav  class="navbar  navbar-light bg-dark">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">
+    <a class="navbar-brand text-white" href="#">
       <img src="https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo.svg" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
       Bootstrap
     </a>
   </div>
 </nav>
+
+<div class="container-fluid">
+        <div class="row">
+
+        <div class="col-8">
+        <div class="card "  style=" width: 40%;" >
+  <img src="https://images.unsplash.com/photo-1718115535371-22e6db1dfee8?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyMHx8fGVufDB8fHx8fA%3D%3D" style=" width: 100%; height: 300px;    object-fit: cover;" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+        </div>
+
+        <div class="col-4"></div>
+        </div>
+</div>
 </div>
 
