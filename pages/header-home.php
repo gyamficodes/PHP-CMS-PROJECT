3c23a7a64836812f526ddf8c267ba88e8bb61632
Gyamfi John <115163376+gyamficodes@users.noreply.github.com>
2024-06-17 06:17:26 -0700

    update

--- pages/header-home.php
+++ pages/header-home.php
@@ -4,5 +4,10 @@
       <img src="https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo.svg" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
       Bootstrap
     </a>
+
+    <div class="d-flex ">
+        <a class="nav-link text-white" href="#">Login</a>
+        <a class="nav-link text-white" href="#">Register</a>
+    </div>
   </div>
 </nav>
