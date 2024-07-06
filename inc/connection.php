115d4faaac13055dddf3d11b5eb0214fd4cfa6cf
gyamficodes <gyamfijohn08@gmail.com>
2024-07-06 20:57:40 +0000

    update

--- inc/connection.php
+++ inc/connection.php
@@ -6,15 +6,15 @@ $host = "localhost";
 $database = "basic_app";
 $user = "root";
 $password = "";
-
+$port = 3306; 
 
 
 //connecting to mysql database
-$connection = mysqli_connect($host , $user ,$password, $database) or die("Database can not connect");
+$connection = mysqli_connect($host , $user ,$password, $database,$port) or die("Database can not connect");
 
 // Check connection
-if (!$connection) {
-    die("Connection failed: " . mysqli_connect_error());
-  }
-  echo "Database Connected successfully";
+// if (!$connection) {
+//     die("Connection failed: " . mysqli_connect_error());
+//   }
+//   echo "Database Connected successfully";
 
